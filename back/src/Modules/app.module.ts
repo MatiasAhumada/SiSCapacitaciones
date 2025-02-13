d82bcccef5cb5f7cdece6052024a67319e5fdea6
Matiasahumada <pagosptcmatias@gmail.com>
2025-02-13 16:00:46 -0300

    cambie el nombre de mayuscula a minuscula a todos

--- back/src/Modules/app.module.ts
+++ back/src/Modules/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlumnoModule } from './Alumno/alumno.module';
-import { AdminModule } from './Admin/admin.module';
+import { AdminModule } from './admin/admin.module';
 import { AbonoModule } from './abono/abono.module';
 import { CursoModule } from './curso/curso.module';
 import { ProfesorModule } from './profesor/profesor.module';
 import { ServicioModule } from './servicio/servicio.module';
 import { VendedorModule } from './vendedor/vendedor.module';
+import { AlumnoModule } from './alumno/alumno.module';
 
 @Module({
   imports: [AlumnoModule, AdminModule, AbonoModule, CursoModule, ProfesorModule, ServicioModule, VendedorModule],

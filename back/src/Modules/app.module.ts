e59c229c692bfa939c0ddd383eb11111d702379f
Matiasahumada <pagosptcmatias@gmail.com>
2025-02-13 15:55:36 -0300

    cree todas las carpetas llenas del module

--- back/src/Modules/app.module.ts
+++ back/src/Modules/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlumnoModule } from './Alumno/alumno.module';
+import { AdminModule } from './Admin/admin.module';
+import { AbonoModule } from './abono/abono.module';
+import { CursoModule } from './curso/curso.module';
+import { ProfesorModule } from './profesor/profesor.module';
+import { ServicioModule } from './servicio/servicio.module';
+import { VendedorModule } from './vendedor/vendedor.module';
 
 @Module({
-  imports: [],
+  imports: [AlumnoModule, AdminModule, AbonoModule, CursoModule, ProfesorModule, ServicioModule, VendedorModule],
   controllers: [AppController],
   providers: [AppService],
 })

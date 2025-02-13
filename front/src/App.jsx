c54d46df126bc70478667acaecdcc67a316d092f
Matiasahumada <pagosptcmatias@gmail.com>
2025-02-13 14:47:59 -0300

    Pude hacer que taildwind v4 funcione

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,13 @@
+import Home from "./components/Views/Home";
 import Login from "./components/Views/Login";
 
-import {  Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login></Login>}></Route>
-        <Route path="/inicio" element={<Login></Login>}></Route>
+        <Route path="/inicio" element={<Home></Home>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- front/src/components/Views/Home.jsx
+++ front/src/components/Views/Home.jsx
@@ -1,11 +1,58 @@
-
+import { Row, Col, Card, Button, Container } from "react-bootstrap";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+      <Row className="justify-content-center">
+        <Col lg={4} sm={4} xs={12} className="d-flex justify-content-center">
+          <Card style={{ width: "18rem" }} className="m-3 text-center">
+            <Card.Body>
+              <Card.Title className="mb-3">Sede Santiago</Card.Title>
+
+              <Card.Subtitle className="mb-3 text-muted">Profesores: 5</Card.Subtitle>
+              <Card.Subtitle className="mb-3 text-muted">Vendedores: 3</Card.Subtitle>
+              <Card.Subtitle className="mb-3 text-muted">Alumnos: 200</Card.Subtitle>
+
+              <div className="mt-4">
+                <Button variant="primary">Ingresar</Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col lg={4} sm={4} xs={12} className="d-flex justify-content-center">
+          <Card style={{ width: "18rem" }} className="m-3 text-center">
+            <Card.Body>
+              <Card.Title className="mb-3">Sede Centro</Card.Title>
+
+              <Card.Subtitle className="mb-3 text-muted">Profesores: 5</Card.Subtitle>
+              <Card.Subtitle className="mb-3 text-muted">Vendedores: 3</Card.Subtitle>
+              <Card.Subtitle className="mb-3 text-muted">Alumnos: 200</Card.Subtitle>
+
+              <div className="mt-4">
+                <Button variant="primary">Ingresar</Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col lg={4} sm={4} xs={12} className="d-flex justify-content-center">
+          <Card style={{ width: "18rem" }} className="m-3 text-center">
+            <Card.Body>
+              <Card.Title className="mb-3">Sede Tafi Viejo</Card.Title>
+
+              <Card.Subtitle className="mb-3 text-muted">Profesores: 5</Card.Subtitle>
+              <Card.Subtitle className="mb-3 text-muted">Vendedores: 3</Card.Subtitle>
+              <Card.Subtitle className="mb-3 text-muted">Alumnos: 200</Card.Subtitle>
+
+              <div className="mt-4">
+                <Button variant="primary">Ingresar</Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;

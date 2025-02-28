8e82a7a192a238f2d46ca96df3648564def14a42
Matiasahumada <pagosptcmatias@gmail.com>
2025-02-28 00:32:27 -0300

    realice el login de titulos co nel back

--- titulos/src/components/Views/Login.jsx
+++ titulos/src/components/Views/Login.jsx
@@ -1,10 +1,31 @@
 import { Link, useNavigate } from "react-router-dom";
 import imagen from "../assets/simplificado_a_color.png";
+import { login } from "../queries/queries";
+import { useState } from "react";
+import Swal from "sweetalert2";
 const Login = () => {
+  const [formData, setFormData] = useState({ name: "", password: "" });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   const navigate = useNavigate();
-  const handleClick = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate("/inicio");
+
+    await login(formData).then((data) => {
+      if (data.isAdmin) {
+        Swal.fire({
+          icon: "success",
+          title: "Inicio de sesión exitoso",
+          showConfirmButton: false,
+          timer: 1500,
+        }).then(() => {
+          navigate("/home");
+        });
+      }
+    });
   };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -17,17 +38,18 @@ const Login = () => {
             <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">Iniciar sesión</h2>
           </div>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-600">
-              Número de D.N.I
+            <label htmlFor="name" className="block text-sm font-medium text-gray-600">
+              Nombre
             </label>
             <input
-              type="email"
-              id="email"
-              name="email"
+              type="string"
+              id="name"
+              name="name"
               className="w-full p-3 border border-gray-300 rounded-md mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Ingresa tu correo"
+              onChange={handleChange}
             />
           </div>
 
@@ -41,10 +63,11 @@ const Login = () => {
               name="password"
               className="w-full p-3 border border-gray-300 rounded-md mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Ingresa tu contraseña"
+              onChange={handleChange}
             />
           </div>
 
-          <button type="submit" onClick={handleClick} className="w-full py-3 btnAz text-white font-semibold rounded  transition duration-200 ">
+          <button type="submit" className="w-full py-3 btnAz text-white font-semibold rounded  transition duration-200 ">
             Iniciar sesión
           </button>
         </form>
--- titulos/src/components/queries/queries.js
+++ titulos/src/components/queries/queries.js
@@ -1,11 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
-const URL ="http://82.29.62.125:4040"
+const URL = "http://82.29.62.125:4040";
 
-export const login =async(user)=>{
-    try {
-        const response = await axios.get(`${URL}/admin`,user)	
-    } catch (error) {
-        
-    }
-}
+export const login = async ({name,password}) => {
+ 
+  try {
+    const response = await axios.post(`${URL}/admin/login`, {name, password });
+    return response.data;
+  } catch (error) {
+    console.error("Error al iniciar sesión", error.response?.data);
+  }
+};

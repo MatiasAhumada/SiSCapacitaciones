24e5c8f9a762a931909497ef7eeea2b5c75bac59
Matiasahumada <pagosptcmatias@gmail.com>
2025-02-28 00:28:10 -0300

    cambie la forma de llamar a cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Modules/app.module';
-import cors from "cors";
+import cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors())
+  app.enableCors({
+    origin: '*', // Cambia esto en producción
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(4040);
 }
 bootstrap();

b0e473aa5eb573641417301d0e7996ea24483c7f
Matiasahumada <pagosptcmatias@gmail.com>
2025-02-28 00:24:14 -0300

    agregeu cors en el back

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Modules/app.module';
-
+import cors from "cors";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors())
   await app.listen(4040);
 }
 bootstrap();

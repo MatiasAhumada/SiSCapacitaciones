8c3078257b0854ec7c0360b904c99a9e52343290
Matiasahumada <pagosptcmatias@gmail.com>
2025-03-19 10:48:58 -0300

    agregue la info de la caja actual en el dash de vendedor

--- front/src/components/vendedor/ventas/InfoIndexVend.jsx
+++ front/src/components/vendedor/ventas/InfoIndexVend.jsx
@@ -1,44 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getVendID } from "../../queris/queris";
+import { GetCajaByVendedor, getVendID } from "../../queris/queris";
 import Swal from "sweetalert2";
 
 const InfoIndexVend = () => {
   const [vendedor, setVendedor] = useState(null);
+  const [totalCaja, setTotalCaja] = useState(0);
+
   useEffect(() => {
     const id = localStorage.getItem("token");
-    getVendID(id).then((data) => {
-      if (data) {
-      console.log(data)  
-        Swal.fire({
-          icon: "success",
-          title: "Bienvenido!",
-          showConfirmButton: false,
-          timer: 1500,
-        }).then(() => {
-          setVendedor(data);
-        });
-      }
-    });
+
+    const vendedor = async () => {
+      await getVendID(id).then((data) => {
+        if (data) {
+          Swal.fire({
+            icon: "success",
+            title: "Bienvenido!",
+            showConfirmButton: false,
+            timer: 1500,
+          }).then(() => {
+            setVendedor(data);
+          });
+        }
+      });
+    };
+
+    const cajaVendedor = async () => {
+      await GetCajaByVendedor(id).then((data) => {
+        try {
+          const total = data.reduce((acc, item) => {
+            const monto = parseFloat(item.monto) || 0;
+            return item.tipo === "transferencia" || item.tipo === "egreso" ? acc - monto : acc + monto;
+          }, 0);
+
+          setTotalCaja(total);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    };
+    vendedor();
+    cajaVendedor();
   }, []);
   const links = [{ name: "Inscribir" }, { name: "Abono" }, { name: "Caja" }, { name: "Comisiones" }];
   const stats = [
     { name: "Inscripciones realizadas", value: vendedor?.inscripciones?.length || "0" },
     { name: "Alumnos registrados", value: "300+" },
-    { name: "Hours per week", value: "40" },
+    { name: "Caja actual", value: `$${totalCaja}` },
     { name: "Paid time off", value: "Unlimited" },
   ];
+
   const navigate = useNavigate();
+
   const click = (name) => {
     navigate(`/inicioVendedor/${name.toLowerCase()}`);
   };
+  
   return (
     <div className="relative isolate overflow-hidden  py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div>
           <h2 className="principal text-5xl mb-3  sm:text-7xl">{vendedor ? vendedor.name : "Cargando..."}</h2>
 
-          <p className="mt-8 sm:text-xl/8">  {vendedor && vendedor.sucursales.length > 0 ? vendedor.sucursales[0].name : "Sin sucursales"}</p>
+          <p className="mt-8 sm:text-xl/8"> {vendedor && vendedor.sucursales.length > 0 ? vendedor.sucursales[0].name : "Sin sucursales"}</p>
         </div>
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
           <div className="grid grid-cols-1 mb-5 gap-x-8 gap-y-6 text-base/7 font-semibold sm:grid-cols-4 md:flex lg:gap-x-40 ">

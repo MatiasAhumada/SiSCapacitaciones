92ef4a1955e67139c13489e7f5cc1276e3f4c0d9
Matiasahumada <pagosptcmatias@gmail.com>
2025-03-19 10:55:08 -0300

    cambio de profesor a movimiento

--- front/src/components/caja/CreateCaja.jsx
+++ front/src/components/caja/CreateCaja.jsx
@@ -256,7 +256,7 @@ const CreateCaja = () => {
               </path>
             </svg>
           ) : (
-            "Registrar Profesor"
+            "Registrar Movimiento"
           )}
         </button>
       </form>

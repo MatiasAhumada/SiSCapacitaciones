f94fab96ff052417b6850948c3fac4959f6a7ef0
MatiasAhumada <pagosptcmatias@gmail.com>
2025-05-01 11:42:23 -0300

    cambio en host del vite.config

--- titulos/vite.config.js
+++ titulos/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 8081,
     strictPort: true,
     allowedHosts: ["certificacionnacional.org"],
